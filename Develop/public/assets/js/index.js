4dd5be2e7bc53f8e1221bb4158164eff908893e7
Lea Wilhelmer <leawilhelmer@gmail.com>
2020-12-19 12:22:04 -0800

    updates

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -25,13 +25,13 @@ var deleteNote = function(id) {
   return $.ajax({
     url: "api/notes/" + id,
     method: "DELETE"
-  });
+  })
 };
 
 var renderActiveNote = function() {
   $saveNoteBtn.hide();
 
-  if (activeNote.id) {
+  if (typeof activeNote.id === "number") {
     $noteTitle.attr("readonly", true);
     $noteText.attr("readonly", true);
     $noteTitle.val(activeNote.title);
@@ -50,27 +50,23 @@ var handleNoteSave = function() {
     text: $noteText.val()
   };
 
-  saveNote(newNote).then(function(data) {
+  saveNote(newNote);
     getAndRenderNotes();
     renderActiveNote();
-  });
 };
 
 var handleNoteDelete = function(event) {
   event.stopPropagation();
 
-  var note = $(this)
-    .parent(".list-group-item")
-    .data();
+  var note = $(this).data('id');
 
-  if (activeNote.id === note.id) {
+  if (activeNote.id === note) {
     activeNote = {};
   }
 
-  deleteNote(note.id).then(function() {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+  deleteNote(note);
+  getAndRenderNotes();
+  renderActiveNote();
 };
 
 var handleNoteView = function() {
@@ -100,9 +96,11 @@ var renderNoteList = function(notes) {
     var note = notes[i];
 
     var $li = $("<li class='list-group-item'>").data(note);
+    $li.data('id',i);
+
     var $span = $("<span>").text(note.title);
     var $delBtn = $(
-      "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+      "<i class='fas fa-trash-alt float-right text-danger delete-note' data-id="+i+">"
     );
 
     $li.append($span, $delBtn);
